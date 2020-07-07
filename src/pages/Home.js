a2d76335dd269b5ea95d758b17d5c3c62ec42687
Mason Burke <masonburke@rocketmail.com>
2020-07-07 14:39:52 -0600

    navbar, footer, icons added.

---
+++ src/pages/Home.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import Navigation from "../components/Navbar"
+import Footer from "../components/Footer"
+
+const Home = () => {
+    return(
+
+<>
+<Navigation />
+
+
+
+
+<Footer />
+</>
+    )
+
+}
+
+
+
+
+export default Home
